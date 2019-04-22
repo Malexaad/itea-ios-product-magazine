5ac152afcbdf56f8b5d0e394939ee4a7dfc58dda
Alex <alexmarfutin@gmail.com>
2019-04-22 23:39:01 +0300

    #Add Login.storyboard #Add LoginViewController #Add UsersDictModel Only design

---
+++ itea-ios-products-magazine/itea-ios-products-magazine/Controllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  itea-ios-products-magazine
+//
+//  Created by Alex Marfutin on 4/22/19.
+//  Copyright © 2019 G9. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ itea-ios-products-magazine/itea-ios-products-magazine/Models/UsersDictModel.swift
@@ -0,0 +1,9 @@
+//
+//  UsersDictModel.swift
+//  itea-ios-products-magazine
+//
+//  Created by Alex Marfutin on 4/22/19.
+//  Copyright © 2019 G9. All rights reserved.
+//
+
+import Foundation
