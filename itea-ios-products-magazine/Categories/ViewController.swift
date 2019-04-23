3589e020f808728b1a5333d76945d9cf246c9be4
Daniel <firecast.gg@gmail.com>
2019-04-23 16:58:05 +0300

    upd

--- itea-ios-products-magazine/Categories/ViewController.swift
+++ itea-ios-products-magazine/Categories/ViewController.swift
@@ -21,9 +21,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let listCell = UINib(nibName: "ListCell", bundle: nil)
         Table.register(listCell, forCellReuseIdentifier: "ListCell")
         
-        var test1 = ItemType.test1
-        var test2 = ItemType.test2
-        var test3 = ItemType.test3
+        _ = ItemType.test1
+        _ = ItemType.test2
+        _ = ItemType.test3
         
         arrayList.append(.test1)
         arrayList.append(.test2)
@@ -39,13 +39,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 77
+        return 107
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = Table.dequeueReusableCell(withIdentifier: "ListCell", for: indexPath) as! ListCell
         cell.update(type: arrayList[indexPath.row])
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     }
 
 }
