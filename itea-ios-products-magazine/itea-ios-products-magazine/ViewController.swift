857991e8ab67c941b58d0e50a0493ebfb890fff1
anatolii90 <a601762847@gmail.com>
2019-04-25 00:35:24 +0300

    #1

---
+++ itea-ios-products-magazine/itea-ios-products-magazine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  itea-ios-products-magazine
+//
+//  Created by Alex Marfutin on 4/22/19.
+//  Copyright © 2019 G9. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
