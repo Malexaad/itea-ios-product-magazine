9ba67523c3cdcb7f378bfd2f3a7d0b86c4d3dac3
Valik Petrulia <valentinpetrulia@gmail.com>
2019-05-15 20:53:39 +0300

    added feedbackArray

--- itea-ios-products-magazine/itea-ios-products-magazine/DetailViewController.swift
+++ itea-ios-products-magazine/itea-ios-products-magazine/DetailViewController.swift
@@ -9,22 +9,53 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    var item: Item?
+    var user: UserInfoModel?
 
+    @IBOutlet weak var avatar: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var shop: UILabel!
+    @IBOutlet weak var category: UILabel!
+    @IBOutlet weak var subcategory: UILabel!
+    @IBOutlet weak var taste: UILabel!
+    @IBOutlet weak var weight: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var reviewsButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.navigationController?.isNavigationBarHidden = true
+        update(item: item!)
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func update(item: Item, user: UserInfoModel) {
+        avatar.image = UIImage(named: user.userFoto)
+        imageView.image = UIImage(named: item.image)
+        name.text = item.name
+        shop.text = item.shop
+        category.text = item.category
+        subcategory.text = item.subcategory
+        weight.text = item.weight
+        price.text = item.price
+        taste.text = item.taste
+        if let vc = UIStoryboard(name: "Feedback", bundle: nil).instantiateInitialViewController() {
+            reviewsButton.setTitle("Отзывы" + "\(vc.feedbackArray.count)", for: .normal)
+        }
     }
-    */
-
+    
+    @IBAction func didTapBackButton(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func didTapReviewButton(_ sender: Any) {
+        if let vc = UIStoryboard(name: "Feedback", bundle: nil).instantiateInitialViewController() {
+            vc.fbProduct = item
+            vc.fbUser = user
+            vc.modalTransitionStyle = .crossDissolve
+            present(vc, animated: true, completion: nil)
+        }
+    }
+    
 }
--- itea-ios-products-magazine/itea-ios-products-magazine/Item.swift
+++ itea-ios-products-magazine/itea-ios-products-magazine/Item.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import UIKit
+
+class Item {
+    var name: String
+    var image: String
+    var shop: String
+    var category: String
+    var subcategory: String
+    var weight: String
+    var price: String
+    var taste: String
+    var feedbackArray: [Feedback]
+    
+    init(name: String, image: String, shop: String, category: String, subcategory: String, weight: String, price: String, taste: String, feedbackArray: [Feedback]) {
+        self.name = name
+        self.image = image
+        self.shop = shop
+        self.category = category
+        self.subcategory = subcategory
+        self.weight = weight
+        self.price = price
+        self.taste = taste
+        self.feedbackArray = feedbackArray
+    }
+}
